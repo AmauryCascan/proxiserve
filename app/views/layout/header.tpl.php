dcbb8f2bc2232e28f39e41b343879fd2dfdbd979
AmauryCascan <amaury.cascan1@oclock.school>
2024-02-12 14:58:02 +0100

    modif js

--- app/views/layout/header.tpl.php
+++ app/views/layout/header.tpl.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="<?=$viewData['assetsBaseUri']?>style.css">
+    <link rel="stylesheet" href="<?= $viewData['assetsBaseUri']?>style.css">
     <title>Proxiserve</title>
 
   </head>
