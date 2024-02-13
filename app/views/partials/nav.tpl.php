05c50becb6f90d64e5ba9da362876f054ed49202
AmauryCascan <amaury.cascan1@oclock.school>
2024-02-13 15:57:55 +0100

    modif de la nav

--- app/views/partials/nav.tpl.php
+++ app/views/partials/nav.tpl.php
@@ -24,6 +24,8 @@
                 </div>
                     <?php if(isset($_SESSION['userObject']) && $_SESSION['userObject']->getStatus() === 1 && strpos($_SERVER['REQUEST_URI'], 'vth') !== false) : ?>
                         <div class="btn-group">
+                            <a class="btn btn-sm btn-outline-dark <?= ($currentPage === "vthTravaux/add") ? "active" : "" ?>" 
+                                    href="<?= $this->router->generate('vth-add');?>">Nouveau Bon</a>
                             <a class="btn btn-sm btn-outline-dark <?= ($currentPage === "vth/encours") ? "active" : "" ?>"
                                  href="<?= $this->router->generate('vth-list');?>">En cours</a>
                             <a class="btn btn-sm btn-outline-dark <?= ($currentPage === "vth/facture") ? "active" : "" ?>" 
@@ -33,22 +35,20 @@
                                 <a class="btn btn-sm btn-outline-dark <?= ($currentPage === "vth/robinetterie") ? "active" : "" ?>" 
                                 href="<?= $this->router->generate('robinetterie-list');?>">Robinetterie</a>
                             <?php if($_SESSION['userObject']->getRole() === "manager" || $_SESSION['userObject']->getRole() === "admin") : ?>
-                                <a class="btn btn-sm btn-outline-dark <?= ($currentPage === "vthTravaux/add") ? "active" : "" ?>" 
-                                    href="<?= $this->router->generate('vth-add');?>">Nouveau Bon</a>
                             <?php endif ?>
                         </div>
                     <?php endif; ?>
                     <?php if(isset($_SESSION['userObject']) && strpos($_SERVER['REQUEST_URI'], 'tl') !== false) : 
                             if($_SESSION['userObject']->getRole() === "manager" || $_SESSION['userObject']->getRole() === "admin") :?>
                             <div class="btn-group">
+                            <a class="btn btn-sm btn-outline-dark <?= ($currentPage === "tlTravaux/add") ? "active" : "" ?>" 
+                                    href="<?= $this->router->generate('tl-add');?>">Nouveau Bon</a> 
                                 <a class="btn btn-sm btn-outline-dark <?= ($currentPage === "tl/encours") ? "active" : "" ?>"
                                      href="<?= $this->router->generate('tl-list');?>">En cours</a>
                                 <a class="btn btn-sm btn-outline-dark <?= ($currentPage === "tl/facture") ? "active" : "" ?>" 
                                     href="<?= $this->router->generate('facturetl-list');?>">Facturé</a>
                                 <a class="btn btn-sm btn-outline-dark <?= ($currentPage === "tl/annule") ? "active" : "" ?>" 
                                     href="<?= $this->router->generate('annuletl-list');?>">Annulé</a>
-                                <a class="btn btn-sm btn-outline-dark <?= ($currentPage === "tlTravaux/add") ? "active" : "" ?>" 
-                                    href="<?= $this->router->generate('tl-add');?>">Nouveau Bon</a> 
                             </div>
                         <?php endif;
                     endif; ?>
